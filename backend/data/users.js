67568184882640acd03f4dacd56eec2ec2da44fb
Yuan-ueng <yuanyueheng2019@163.com>
2023-12-13 21:18:26 -0800

    fix data

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,19 +2,19 @@ import bcrypt from "bcryptjs";
 
 const users = [
     {
-        name: 'admin', email: 'admin@gmail.com', password: bcrypt.hashSync('admin', 10), role: 'ADMIN'
+        name: 'Admin', email: 'admin@gmail.com', password: bcrypt.hashSync('admin', 10), role: 'ADMIN'
     },
     {
-        name: 'austen', email: 'jane@gmail.com', password: bcrypt.hashSync('austen', 10), role: 'SELLER'
+        name: 'Austen', email: 'jane@gmail.com', password: bcrypt.hashSync('austen', 10), role: 'SELLER'
     },
     {
-        name: 'beyle', email: 'henri@gmail.com', password: bcrypt.hashSync('beyle', 10), role: 'SELLER'
+        name: 'Beyle', email: 'henri@gmail.com', password: bcrypt.hashSync('beyle', 10), role: 'SELLER'
     },
     {
-        name: 'camus', email: 'albert@gmail.com', password: bcrypt.hashSync('camus', 10), role: 'BUYER'
+        name: 'Camus', email: 'albert@gmail.com', password: bcrypt.hashSync('camus', 10), role: 'BUYER'
     },
     {
-        name: 'wilde', email: 'oscar@gmail.com', password: bcrypt.hashSync('wilde', 10), role: 'BUYER'
+        name: 'Wilde', email: 'oscar@gmail.com', password: bcrypt.hashSync('wilde', 10), role: 'BUYER'
     },
 ];
 
